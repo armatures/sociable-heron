8171208eae823879fbed97b27a4f311618657c8b
Charlie Bevis <charliebevis@gmail.com>
2017-11-03 11:40:03 -0500

    cipher doesn't mess with spaces

--- src/Ch11.hs
+++ src/Ch11.hs
@@ -2,13 +2,12 @@ module Ch11 where
 import Data.Char
 
 cipherCaesar :: Char -> String -> String
-cipherCaesar shift' message =
-  map ( chr . ((+) aNum) . (flip mod 25) . ((+) shift) . ((+) (negate aNum)) . ord ) message
+cipherCaesar shift' =
+  unwords . ((map . map) letterCipher) . words
     where
       aNum = ord 'a'
-      zNum = ord 'z'
       shift = (ord . toLower) shift' - aNum
-      rankLetter = (((-) aNum) . ord )
+      letterCipher = ( chr . ((+) aNum) . (flip mod 25) . ((+) shift) . ((+) (negate aNum)) . ord )
 
 cipher :: String -> String -> String
 cipher keyword message = undefined
