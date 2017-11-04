6f05d6aee096f09f5af553b33e4afe057d2959ba
Charlie Bevis <charliebevis@gmail.com>
2017-11-04 11:44:31 -0500

    extract reusable function for individual letters

--- src/Ch11.hs
+++ src/Ch11.hs
@@ -3,11 +3,12 @@ import Data.Char
 
 cipherCaesar :: Char -> String -> String
 cipherCaesar shift' =
-  unwords . ((map . map) letterCipher) . words
+  unwords . ((map . map) (shiftLetter shift')) . words
+
+shiftLetter shift' = ( chr . ((+) aNum) . (flip mod 25) . ((+) shift) . ((+) (negate aNum)) . ord )
     where
       aNum = ord 'a'
       shift = (ord . toLower) shift' - aNum
-      letterCipher = ( chr . ((+) aNum) . (flip mod 25) . ((+) shift) . ((+) (negate aNum)) . ord )
 
 cipher :: String -> String -> String
 cipher keyword message = undefined
