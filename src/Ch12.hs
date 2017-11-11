72fd4b8853b347a97dd95ca090b9617d4d9db243
Charlie Bevis <charliebevis@gmail.com>
2017-11-11 09:43:54 -0600

    take better advantage of the Maybe type with the fromMaybe function

--- src/Ch12.hs
+++ src/Ch12.hs
@@ -1,18 +1,15 @@
 module Ch12 where
-import Data.Maybe 
+import Data.Maybe
+
 replaceThe:: String -> String
-replaceThe = unwords . map replace . words
-  where replace s =
-          case notThe s of
-            Just word -> word
-            Nothing -> "a"
+replaceThe = unwords . map (fromMaybe "a" . notThe) . words
 
 notThe:: String -> Maybe String
 notThe s | s == "the" = Nothing
          | otherwise = Just s
 
 countTheBeforeVowel :: String -> Integer
-countTheBeforeVowel = fst . (foldl countHelp (0,False)) . words
+countTheBeforeVowel = fst . (foldl countHelp (0, False)) . words
   where
     countHelp (count, isFollowingThe) word =
       if isFollowingThe && elem (head word) "aeiou" then
