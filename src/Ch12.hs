d08fc908b521040508d9cd68e1a9bd0181ca22fe
Charlie Bevis <charliebevis@gmail.com>
2017-11-11 13:54:03 -0600

    signaling adversity, part 2

--- src/Ch12.hs
+++ src/Ch12.hs
@@ -1,5 +1,5 @@
 module Ch12 where
-
+import Data.Maybe 
 replaceThe:: String -> String
 replaceThe = unwords . map replace . words
   where replace s =
@@ -11,4 +11,11 @@ notThe:: String -> Maybe String
 notThe s | s == "the" = Nothing
          | otherwise = Just s
 
-
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel = fst . (foldl countHelp (0,False)) . words
+  where
+    countHelp (count, isFollowingThe) word =
+      if isFollowingThe && elem (head word) "aeiou" then
+        (count + 1, False)
+      else
+        (count, isNothing $ notThe word)
