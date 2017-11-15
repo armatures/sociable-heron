25f384fe40361401b833825b9b6c3a78f9fe27f8
Charlie Bevis <charliebevis@gmail.com>
2017-11-15 05:15:02 -0600

    Anamorphisms! What fun!

--- src/Ch12.hs
+++ src/Ch12.hs
@@ -49,3 +49,17 @@ countVowelsAndConsonants (vowels,consonants) x =
     (vowels+1, consonants)
   else
     (vowels, consonants+1)
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f x =
+  x:myIterate f (f x)
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f x = case f x of
+  Nothing -> []
+  Just (y,z) -> y:myUnfoldr f z
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f =
+  myUnfoldr (\y -> Just (y,f y))
+
